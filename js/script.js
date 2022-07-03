18a2bd03037095f60ebc0721d8389bfa26f336b2
LanaKasparova <kasparovalana@gmail.com>
2022-07-03 19:12:27 +0200

    js hw 2
    lesson 1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//Task 1
+
+const name='John';
+let admin=(name);
+alert(admin);
+
+//Task 2
+
+let ourPlanet='Earth';
+const currentUser=prompt('What is your name?');
+alert(currentUser);
+
+// Task 3
+
+const type = typeof ('10' + 10);
+console.log(type);
